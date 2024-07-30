9e4ca4249db7539058d3fdd4b1dae05aff57109b
victormuteithia <muteithiavictor@gmail.com>
2024-07-30 13:16:33 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("#create-task-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    handleSubmit(e.target.new_task_description.value)
+    form.reset()
+  })
+  
 });
+
+function color(priority){
+  switch(priority){
+    case "high_priority":
+      return "red";
+    case "medium":
+      return "yellow";
+    case "low":
+      return "green";
+  }
+}
+
+
+function handleSubmit(todo){
+  let li = document.createElement("li");
+  let btn = document.createElement("button");
+  let selectorValue = document.querySelector("#priority").value
+  btn.textContent = "x";
+  btn.addEventListener("click", handleDelete)
+  li.style.color = color(selectorValue);
+  li.textContent = `${todo} `;
+  li.appendChild(btn)
+  let ul = document.querySelector("#tasks")
+  ul.appendChild(li);
+  
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
+
+
+
